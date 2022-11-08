d4dd33a4acdd08d934659b4a1508a41d71b79381
Mark Gowen <mark.gowen78@gmail.com>
2022-11-08 09:44:01 -0800

    Unresolved solution

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const target = document.getElementsByClassName('like-glyph');
 
+function likeSomething(e) {
+  const hidden = document.querySelector('.hidden')
+  const modalMessage = document.querySelector("#modal-message")
+
+  console.log(e)
+  return mimicServerCall()
+  .then(() => {
+    if (e.target.textContent === EMPTY_HEART) {
+      e.target.textContent = FULL_HEART
+      e.target.classList.add ("activated-heart")
+    } else {
+      e.target.textContent = EMPTY_HEART
+      e.target.classList.remove ("activated-heart")
+    }
+  })
+  .catch((error) => {
+    hidden.className = ""
+    modalMessage.textContent = error;
+    setTimeout(() => {
+      hidden.className = "hidden"
+    }, 3000)
+  });
+}
+
+for (const element of target) {
+  element.addEventListener("click", likeSomething)
+}
 
 
 
@@ -23,3 +51,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
